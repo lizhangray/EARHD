bfadfc577421dc12883ab7d43cb6cb69accdd1e9
lizhangray <37239596+lizhangray@users.noreply.github.com>
2024-07-14 13:15:53 +0800

    Delete script.bash

--- script.bash
+++
@@ -1,7 +0,0 @@
-# python main.py --name edge-nodownscale-fra --dataset_name 2020NHHAZE --sample_interval 300 --model net --h_size 512 --w_size 512 --epochs 1000 --lambda_edge 0
-# python main.py --name siglePretrainedBranch --dataset_name 2018OHAZE --test_only --model siglePretrainedBranch
-
-python main.py \
---name 2020NHHAZE_Split_Aug \
---model sym \
---comment RNet_DNet_best\
